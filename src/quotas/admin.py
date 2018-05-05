69cf306d10fb963a7a8f1b50e5dc6efef32b134c
GregLeBarbar <gregory.charmier.epfl.ch>
2018-05-05 11:31:57 +0200

    add models to admin

--- src/quotas/admin.py
+++ src/quotas/admin.py
@@ -1,6 +1,6 @@
 """(c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2018"""
 from django.contrib import admin
-from quotas.models import MyVMQuota
+from quotas.models import MyVMQuota, FacultyAdmin, ItsAdmin
 
 
 class MyVMQuotaAdmin(admin.ModelAdmin):
@@ -9,3 +9,17 @@ class MyVMQuotaAdmin(admin.ModelAdmin):
 
 
 admin.site.register(MyVMQuota, MyVMQuotaAdmin)
+
+
+class FacultyAdminAdmin(admin.ModelAdmin):
+    """ To manage MyVMQuotas in Django-Admin """
+    pass
+
+admin.site.register(FacultyAdmin, FacultyAdminAdmin)
+
+
+class ItsAdminAdmin(admin.ModelAdmin):
+    """ To manage MyVMQuotas in Django-Admin """
+    pass
+
+admin.site.register(ItsAdmin, ItsAdminAdmin)
