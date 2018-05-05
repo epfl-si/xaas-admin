889039f306d48cb5859776806e3d8782416d4987
GregLeBarbar <gregory.charmier.epfl.ch>
2018-05-05 11:31:30 +0200

    Add redirect / to /admin

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,7 @@
 """(c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2018"""
 from django.conf.urls import url
 from django.contrib import admin
+from django.shortcuts import redirect
 from django_tequila.urls import urlpatterns as django_tequila_urlpatterns
 from django_tequila.admin import TequilaAdminSite
 
@@ -8,6 +9,7 @@ admin.autodiscover()
 admin.site.__class__ = TequilaAdminSite
 
 urlpatterns = [
+    url(r'^$', lambda _: redirect('admin:index'), name='index'),
     url('admin/', admin.site.urls),
 ]
 
