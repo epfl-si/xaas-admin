6eb2fcfc10ed197fe9bed1de6c15cea239ff9ff3
GregLeBarbar <gregory.charmier.epfl.ch>
2018-05-05 11:32:36 +0200

    PEP8

--- src/quotas/admin.py
+++ src/quotas/admin.py
@@ -15,11 +15,13 @@ class FacultyAdminAdmin(admin.ModelAdmin):
     """ To manage MyVMQuotas in Django-Admin """
     pass
 
+
 admin.site.register(FacultyAdmin, FacultyAdminAdmin)
 
 
 class ItsAdminAdmin(admin.ModelAdmin):
     """ To manage MyVMQuotas in Django-Admin """
     pass
+
 
 admin.site.register(ItsAdmin, ItsAdminAdmin)
