6aba245cbd20fa1320b4142633a2829b757961b3
GregLeBarbar <gregory.charmier.epfl.ch>
2018-05-10 07:43:09 +0200

    config test server

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,10 @@
 # coding: utf-8
 
 """
-(c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2018
-"""
-from tempfile import mkdtemp
-from contextlib import contextmanager as _contextmanager
+    (c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017
 
-from fabric.colors import red
-from fabric.utils import abort
-from fabric.api import run, sudo, cd, lcd, local, prompt, prefix, put
-from fabric.state import env
-
-
-"""
 fab prod deploy
-  Deploy the 'master' branch of the 'origin' repository on the node 1 of production server.
+  Deploy the 'master' branch of the 'origin' repository on the production server.
   The local repository is pushed to the 'origin' before beign deployed.
 
 fab test deploy
@@ -26,8 +16,17 @@ fab clone
 
 fab test clone
   Clone the production environment on the test server.
-
 """
+
+import os
+
+from fabric.api import run, sudo, cd, lcd, local, prompt, prefix, put, settings
+from tempfile import mkdtemp
+from contextlib import contextmanager as _contextmanager
+from fabric.utils import abort
+from fabric.colors import red
+from fabric.state import env
+from fabric.contrib.files import exists
 
 
 # default rsync filters used when synchronizing python source files
@@ -50,45 +49,38 @@ env.default_rsync_flags = [
 
 # connection string to the production server
 env.prod_host_string = 'kis@.epfl.ch'
-env.prod_host_string_db = 'kis@.epfl.ch'
 
-SERVER_APACHE = 'apache'
+env.mysql_db = 'xaas-admin'
+env.mysql_user = 'xaas-admin'
+env.mysql_pwd = ''
+env.mysql_host = 'localhost'
 
-env.server = SERVER_APACHE
-
+# Shell command that dump the production database.
+# This command will be run on the local machine and should dump the production database on stdout
+# so that it can be piped to other commands.
 env.repo_path = '/home/kis/xaas-admin'
 env.vhost_path = '/var/www/vhosts/xaas-admin.epfl.ch'
 env.public_path = '%(vhost_path)s/htdocs' % env
 env.upload_path = '%(public_path)s/upload' % env
 env.private_path = '%(vhost_path)s/private' % env
-
 env.source_path = '%(private_path)s/src' % env
-env.settings_path = '%(source_path)s/config/settings' % env
 env.requirements_path = '%(private_path)s/requirements' % env
-env.docs_path = '%(public_path)s/docs' % env
-
-env.virtualenv_path = '/home/kis/xaas-admin' % env
+env.settings_path = '%(source_path)s/config/settings' % env
+env.virtualenv_path = '%(private_path)s/virtenv/xaas-admin-env' % env
 env.apache_conf_path = '%(vhost_path)s/conf' % env
+env.python_path = '/opt/rh/rh-python35' % env
+
+env.prod_db_dump_cmd = 'ssh %(prod_host_string)s \
+                       "mysqldump -u %(mysql_user)s -p%(mysql_pwd)s  \
+                       -h%(mysql_host)s %(mysql_db)s"' % env
 
 env.files_to_copy = [
-    ('robots.txt', env.public_path),
+    ('xaas-admin.conf', env.apache_conf_path),
 ]
-
-# Shell command that dump the production database.
-# This command will be run on the local machine and should dump the production database on stdout
-# so that it can be piped to other commands.
-env.prod_db_dump_cmd = "ssh %(prod_host_string)s \
-'mysqldump $(source %(virtualenv_path)s/bin/activate; python %(source_path)s/manage.py dbparams)'" % env
 
 
 def help():
     print(globals()['__doc__'])
-
-
-def print_error(str):
-    print("*********************************")
-    print(str)
-    print("*********************************")
 
 
 def test():
@@ -100,8 +92,16 @@ def test():
     env.deploy_local_repository = True
     env.master_branch_only = False
     env.host_string = 'kis@exopgesrv34.epfl.ch'
-    env.requirements = '%(private_path)s/requirements/test.txt' % env
     env.settings_file = 'test.py'
+    env.requirements = '%(requirements_path)s/test.txt' % env
+
+    # # when deploying on the test server, we synchronize the static files of the homepage
+    # # only on the test server of www.epfl.ch
+    # env.deploy_statics_cmds = [
+    #     "rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ /var/www/vhosts/www.epfl.ch/htdocs/public/hp2013/",
+    #     "RSYNC_PASSWORD=" + django_manage("get_secrets RSYNC_PASSWORD_PROD") + " rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ wwwsync@exopgesrv95::rw.epfl.ch/htdocs/public/hp2013/",  # noqa
+    #     "RSYNC_PASSWORD=" + django_manage("get_secrets RSYNC_PASSWORD_PROD") + " rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ wwwsync@exopgesrv96::rw.epfl.ch/htdocs/public/hp2013/",  # noqa
+    # ]
 
 
 def prod():
@@ -113,10 +113,19 @@ def prod():
     env.deploy_local_repository = False
     env.master_branch_only = True
     env.host_string = 'kis@.epfl.ch'
-    env.requirements = '%(private_path)s/requirements/prod.txt' % env
-    env.settings_file = 'prod.py'
-    # Use SSH forwarding to access c4science
     env.forward_agent = True
+    env.settings_file = 'prod.py'
+    env.requirements = '%(requirements_path)s/prod.txt' % env
+
+    # # when deploying on the production server, we synchronize the static files of the homepage
+    # # on both the test and production server of www.epfl.ch
+    # env.deploy_statics_cmds = [
+    #     "RSYNC_PASSWORD=" + django_manage("get_secrets RSYNC_PASSWORD_TEST") + " rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ wwwsync@exopgesrv34::www.epfl.ch/htdocs/public/hp2013/",  # noqa
+    #     "RSYNC_PASSWORD=" + django_manage("get_secrets RSYNC_PASSWORD_PROD") + " rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ wwwsync@exopgesrv95::rw.epfl.ch/htdocs/public/hp2013/",  # noqa
+    #     "RSYNC_PASSWORD=" + django_manage("get_secrets RSYNC_PASSWORD_PROD") + " rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ wwwsync@exopgesrv96::rw.epfl.ch/htdocs/public/hp2013/",  # noqa
+    #     "RSYNC_PASSWORD=" + django_manage("get_secrets RSYNC_PASSWORD_PROD") + " rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ wwwsync@exopgesrv75::rw.epfl.ch/htdocs/public/hp2013/",  # noqa
+    #     "RSYNC_PASSWORD=" + django_manage("get_secrets RSYNC_PASSWORD_PROD") + " rsync -r /var/www/vhosts/homepage.epfl.ch/htdocs/hp2013/ wwwsync@exopgesrv76::rw.epfl.ch/htdocs/public/hp2013/",  # noqa
+    # ]
 
 
 @_contextmanager
@@ -124,17 +133,10 @@ def virtualenv():
     """
     Context manager that activate the virtualenv defined in env.virtualenv_path
     """
-    with prefix('source /opt/rh/rh-python35/enable;source %(virtualenv_path)s/bin/activate' % env):
-        yield
-
-
-def fix_selinux_permissions():
-    for file in ['_mysql.so', 'lxml/*.so*', 'PIL/*.so*', 'PIL/.libs/*.so*', '_ldap.so']:
-        run("chcon -t httpd_sys_script_exec_t %s/lib/python3.5/site-packages/%s" %
-            (env.virtualenv_path, file))
-
-    for folder in ['upload/fckeditorimage/', 'upload/news/']:
-        run("chcon -R -t httpd_sys_rw_content_t /var/www/vhosts/actu.epfl.ch/htdocs/" + folder)
+    with cd(env.source_path):
+        with prefix('source %(python_path)s/enable' % env):
+            with prefix('source %(virtualenv_path)s/bin/activate' % env):
+                yield
 
 
 def django_manage(cmd):
@@ -144,7 +146,7 @@ def django_manage(cmd):
     :param cmd string: the parameters to be passed to manage.py
     """
     with virtualenv():
-        run('python manage.py ' + cmd)
+        return run('python -W ignore manage.py ' + cmd)
 
 
 def clone_local_repo():
@@ -155,21 +157,19 @@ def clone_local_repo():
     Return the path of the local copy.
     """
     local_path = mkdtemp()
-    local('git clone --recursive . "%s"' % local_path)
+    local('git clone . "%s"' % local_path)
 
     return local_path
 
 
 def restart_http_server():
     """
-    Restart the HTTP server on the remote server.
+    Restart the Apache server on the remote server.
     """
-    if env.server == SERVER_APACHE:
-        sudo('/opt/rh/httpd24/root/usr/sbin/apachectl restart ', shell=False)
+    sudo('systemctl restart httpd24-httpd', shell=False)
 
 
-def rsync(src_path, dest_path, flags=env.default_rsync_flags,
-          filters=env.default_rsync_filters):
+def rsync(src_path, dest_path, flags=env.default_rsync_flags, filters=env.default_rsync_filters):
     """
     Return the shell command for executing a rsync.
     You should use 'run(rsync(...))' to execute rsync from the remote server,
@@ -195,18 +195,13 @@ def update_sources_from_local():
     The local repository is cloned in a temporary folder in order
     to ignore uncommited local changes.
     """
-    if env.server == SERVER_APACHE:
-        env.files_to_copy.append(('xaas-admin.conf', env.apache_conf_path))
-
     local_path = clone_local_repo()
-
     local(rsync(local_path + '/src/', '%(host_string)s:%(source_path)s/' % env))
     local(rsync(local_path + '/requirements/', '%(host_string)s:%(requirements_path)s/' % env))
 
-    # Create symlink to use the right settings file
-    # run("ln -sf  %(settings_path)s/%(settings_file)s %(settings_path)s/default.py" % env)
+    run("ln -sf  %(settings_path)s/%(settings_file)s %(settings_path)s/default.py" % env)
 
-    # Copy the various configuration files on the server
+    # copy the various configuration files on the server
     with(lcd(local_path)):
         for file_path, dest_path in env.files_to_copy:
             put(file_path, dest_path)
@@ -226,7 +221,6 @@ def update_sources_from_origin_repo():
     # update the repository on the server
     with(cd(env.repo_path)):
         run('git pull')
-        run('git submodule update --init --remote')
 
     run(rsync('%(repo_path)s/src/' % env, '%(source_path)s/' % env))
     run(rsync('%(repo_path)s/requirements/' %
@@ -253,8 +247,7 @@ def update_sources():
 
 def update_virtualenv():
     with virtualenv():
-        with cd(env.private_path):
-            run('pip install -r %(requirements)s' % env)
+        run('pip install -r %(requirements)s' % env)
 
 
 def get_active_branch():
@@ -272,36 +265,61 @@ def is_ready_or_abort():
 
     If one of the above failed, the deploy script is aborted.
     """
-
     active_branch = get_active_branch()
 
     if env.master_branch_only and active_branch != 'master':
-        abort(red("Only the 'master' branch is allowed. You are on the '%s' branch." % (
-            active_branch, )))
+        abort(red("Only the 'master' branch is allowed. You are on the '%s' branch." % (active_branch, )))
 
     if env.must_confirm:
-        rep = prompt(
-            "Deploy on the '%(role)s' server %(host_string)s ? [type 'yes' to confirm]" % env)
+        rep = prompt("Deploy on the '%(role)s' server %(host_string)s ? [type 'yes' to confirm]" % env)
         if rep != 'yes':
             abort('bye bye')
+
+
+def create_virtenv_if_not_exist():
+    if not exists('%(virtualenv_path)s' % env):
+        with prefix('source %(python_path)s/enable' % env):
+            with cd('%(private_path)s/virtenv' % env):
+                run('virtualenv xaas-admin-env')
+
+
+def fix_permissions():
+    for file in ['*.so', 'lxml/*.so*', 'PIL/*.so*']:
+        files = run("find %s/lib/python3.5/site-packages/%s -type f" % (env.virtualenv_path, file)).splitlines()
+        if (len(files) > 0):
+            run("chcon -t httpd_sys_script_exec_t %s/lib/python3.5/site-packages/%s" %
+                (env.virtualenv_path, file))
+
+    #if env.role == 'test':
+        #run("chown kis:apache /var/www/vhosts/www.epfl.ch/htdocs/xaas-admin/")
+        #run("chmod ug+rw /var/www/vhosts/www.epfl.ch/htdocs/xaas-admin/")
+        #run("chcon -t httpd_sys_rw_content_t /var/www/vhosts/www.epfl.ch/htdocs/xaas-admin/")
+
+    #with settings(warn_only=True):
+    #     run("chown kis:apache /var/www/vhosts/homepage.epfl.ch/private/homepages/")
+    #     run("chmod ug+rw /var/www/vhosts/homepage.epfl.ch/private/homepages/")
+    #     run("chcon -t httpd_sys_rw_content_t /var/www/vhosts/homepage.epfl.ch/private/homepages/")
+    #     run("chown -R kis:apache /var/www/vhosts/homepage.epfl.ch/htdocs/upload/")
+    #     run("chmod -R ug+rw /var/www/vhosts/homepage.epfl.ch/htdocs/upload/")
+    #     run("chcon -R -t httpd_sys_rw_content_t /var/www/vhosts/homepage.epfl.ch/htdocs/upload/")
+
+
+
+
+
 
 
 def deploy():
 
     is_ready_or_abort()
-    print("is_ready_or_abort() OK")
-
     update_sources()
-    print("update_sources() OK")
-
     update_virtualenv()
-    print("update_virtualenv OK")
-
-    # fix_selinux_permissions()
-    # print("fix_selinux_permissions() OK")
-
+    #fix_permissions()
+    #django_manage('compilemessages')
+    #django_manage('update_epfl_header')
+    #django_manage('collectstatic --noinput')
+    #django_manage('daily')
     restart_http_server()
-    print("restart_http_server() OK")
 
 
 def clone():
@@ -314,22 +332,17 @@ def clone():
 
     > fab test clone
     """
+
     if env.host_string is None:
         # clone the production database to the the local machine
-        local(
-            '%(prod_db_dump_cmd)s | mysql -u actu -pactu actu -h 127.0.0.1' %
-            env)
+        local('%(prod_db_dump_cmd)s | mysql -u homepage -phomepage homepage' % env)
     else:
         # check that the remote server is not the production server itself
         if env.role == 'production':
             abort('Cannot clone the database on the production server !')
 
-        else:
-            env.host_mysql_cmd = "ssh %(host_string)s \
-            'mysql $(source %(virtualenv_path)s/bin/activate; python %(source_path)s/manage.py dbparams)'" % env
-            # clone the production database to a remote server
-            local(
-                '%(prod_db_dump_cmd)s | %(host_mysql_cmd)s' % env)
+        # clone the production database to a remote server
+        local('%(prod_db_dump_cmd)s | ssh %(host_string)s "mysql -u homepage -phomepage homepage"' % env)
 
     rsync_flags = [
         '-r',  # recurse into directories
@@ -345,16 +358,19 @@ def clone():
     # Due to security policy, we cannot rsync from production to any remote server directly.
     # We first rsync in a local folder, and later rsync the local folder to the remote
     # server if necessary
-    local(rsync('%(prod_host_string)s:%(upload_path)s/*' %
-                env, 'public/upload', flags=rsync_flags, filters=None))
+    with settings(warn_only=True):
+        result = local(
+            rsync(
+                '%(prod_host_string)s:%(upload_path)s/*' % env,
+                'public/upload', flags=rsync_flags, filters=None
+            )
+        )
+    if result.failed:
+        print('Some files could not be copied')
 
     if env.host_string is not None:
         rsync_flags += [
             '--chmod=ug+rw'  # change file permission on the fly.... je sais plus pourquoi !
         ]
 
-        local(rsync('public/upload/*', '%(host_string)s:%(upload_path)s' %
-                    env, flags=rsync_flags, filters=None))
-
-    if env.host_string == 'kis@actu1.epfl.ch' or env.host_string == 'kis@actu2.epfl.ch':
-        run("chown -R kis:apache /var/www/vhosts/actu.epfl.ch/htdocs/upload")
+        local(rsync('public/upload/*', '%(host_string)s:%(upload_path)s' % env, flags=rsync_flags, filters=None))
--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -1,16 +1,16 @@
 import os
 import sys
 
-activate_this = '/home/kis/xaas-admin/bin/activate_this.py'
+activate_this = '/var/www/vhosts/xaas-admin.epfl.ch/private/virtenv/xaas-admin-env/bin/activate_this.py'
 exec(open(activate_this).read(), dict(__file__=activate_this))
 
 path = '/var/www/vhosts/xaas-admin.epfl.ch/private/src'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.test'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.default'
 os.environ['TMPDIR'] = '/var/www/vhosts/xaas-admin.epfl.ch/private/tmpffi'
-# os.environ['GEM_HOME'] = '/var/www/vhosts/homepage.epfl.ch/private/ruby/'
+os.environ['GEM_HOME'] = '/var/www/vhosts/xaas-admin.epfl.ch/private/ruby/'
 
 
 from django.core.wsgi import get_wsgi_application  # noqa
