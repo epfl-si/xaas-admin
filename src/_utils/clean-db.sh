9731082672d18638ad91593c1f987290073b2ddb
LuluTchab <l.chaboudez@gmail.com>
2018-06-07 08:21:42 +0200

    Move DB info reading (from secrets.json) in another file

--- src/_utils/clean-db.sh
+++ src/_utils/clean-db.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-SECRETS_FILE="${SCRIPT_PATH}/../../secrets.json"
-DB_USER="xaas_admin_user"
+source ".utils-settings.sh"
 
 
 function printUsage
@@ -11,14 +9,6 @@ function printUsage
     echo "Usage: $0 <dbName>"
     echo ""
     echo "   <dbName> : Database name to clean"
-
-}
-
-function extractSecretValue
-{
-    secretKey=$1
-
-    cat ${SECRETS_FILE} | grep ${secretKey} | awk '{print $2}' | awk -F\" '{print $2}'
 
 }
 
@@ -30,23 +20,15 @@ then
     exit 1
 fi
 
-# Extracting DB Password from file
-dbPass=`extractSecretValue "DB_USER_PWD"`
-dbPort=`extractSecretValue "DB_PORT"`
-dbHost=`extractSecretValue "DB_HOST"`
-
-if [ "${dbHost}" == "" ]
-then
-    dbHost="localhost"
-fi
+DB_NAME=$1
 
 # Getting tables list
-TABLES=$(mysql --host=${dbHost} --port=${dbPort} --user=${DB_USER} $1 --password=${dbPass} --execute='show tables' | awk '{ print $1}' | grep -v '^Tables' )
+TABLES=$(mysql --host=${DB_HOST} --port=${DB_PORT} --user=${DB_USER} ${DB_NAME} --password=${DB_PASS} --execute='show tables' | awk '{ print $1}' | grep -v '^Tables' )
 
 for t in $TABLES
 do
-	echo "Deleting $t table from $1 database..."
-	mysql --host=${dbHost} --port=${dbPort} --user=${DB_USER} $1 --password=${dbPass} --execute="drop table $t"
+	echo "Deleting ${t} table from ${DB_NAME} database..."
+	mysql --host=${DB_HOST} --port=${DB_PORT} --user=${DB_USER} ${DB_NAME} --password=${DB_PASS} --execute="drop table ${t}"
 done
 echo "done"
 
