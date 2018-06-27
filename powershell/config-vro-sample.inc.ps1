6f9f1bd1234a57ce9d89b9a42d283730096a2558
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2018-06-27 15:55:00 +0200

    Update global const name... was wrong

--- powershell/config-vro-sample.inc.ps1
+++ powershell/config-vro-sample.inc.ps1
@@ -15,7 +15,7 @@
 # ID de 'cafe' en fonction de l'environnement
 # Peut être trouvé avec la commande suivante passée sur la console SSH de l'appliance vRA :
 # grep -i cafe_cli= /etc/vcac/solution-users.properties | sed -e 's/cafe_cli=//'
-$global:VRA_SERVER_LIST = @{}
-$global:VRA_SERVER_LIST[$global:TARGET_ENV_DEV]	 = ""
-$global:VRA_SERVER_LIST[$global:TARGET_ENV_TEST] = ""
-$global:VRA_SERVER_LIST[$global:TARGET_ENV_PROD] = ""
+$global:VRO_CAFE_CLIENT_ID = @{}
+$global:VRO_CAFE_CLIENT_ID[$global:TARGET_ENV_DEV]	 = ""
+$global:VRO_CAFE_CLIENT_ID[$global:TARGET_ENV_TEST] = ""
+$global:VRO_CAFE_CLIENT_ID[$global:TARGET_ENV_PROD] = ""
