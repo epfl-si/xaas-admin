ce15be42b70f906c1a540f938a9320f6a9d8edee
LuluTchab <l.chaboudez@gmail.com>
2019-03-14 15:49:07 +0100

    Add some comment

--- djangoapp/docker-entrypoint.sh
+++ djangoapp/docker-entrypoint.sh
@@ -27,6 +27,7 @@ fi
 echo "Waiting for MariaDB..."
 
 # We try to connect to database and if unsuccessful, we try again a few seconds later...
+# Note: MySQL host is automatically taken from env var.
 while ! mysql --protocol TCP -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -P"${MYSQL_PORT}" -e "show databases;" > /dev/null 2>&1; do
     sleep 3
     echo "Waiting for MariaDB..."
