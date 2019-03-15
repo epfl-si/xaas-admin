e4b2b974c235af41dd28585f91c99f859c33fd46
LuluTchab <l.chaboudez@gmail.com>
2019-03-15 12:51:22 +0100

    Using build arg and dedicated script to do maximum during image build

--- djangoapp/docker-entrypoint.sh
+++ djangoapp/docker-entrypoint.sh
@@ -1,51 +1,5 @@
 #!/bin/bash
 
-# PIP Install
-#WITNESS_PIP_INSTALL_FILE=/tmp/pip-install-done
-#
-#if [ ! -e ${WITNESS_PIP_INSTALL_FILE} ]
-#then
-#
-#    reqFile=`echo ${DJANGO_SETTINGS_MODULE} | awk -F. '{print $3".txt"}'`
-#
-#    echo "Updating modules using pip (${reqFile})... "
-#
-#    pip install --no-cache-dir -r /tmp/${reqFile}
-#
-#
-#    touch ${WITNESS_PIP_INSTALL_FILE}
-#fi
-#
-#
-# Application source
-TMP_APP_SOURCE_FILES=/tmp/xaas-admin
-WITNESS_FILE=/tmp/source-done
-
-# If this is first execution of container,
-if [ ! -e ${WITNESS_FILE} ]
-then
-    # We set correct location for app source files
-    echo "Setting correct source for Django app..."
-
-    # If we have to use external source files for app (development purpose),
-    if [ "${DJANGO_SETTINGS_MODULE}" = "config.settings.local" ]
-    then
-        echo "-> Using external source for app, deleting what was copied..."
-        rm -rf ${TMP_APP_SOURCE_FILES}
-
-    else # We have to use internal source (Test or production, typically on OpenShift)
-
-        echo "-> Using internal source for app, copying to correct place..."
-        cp -r ${TMP_APP_SOURCE_FILES} /usr/src/
-
-    fi
-
-    # We add the witness file right after. In fact there's an error on OpenShift (permission denied) if
-    # we try to just move source files from one location to another... so we use witness file.
-    touch ${WITNESS_FILE}
-
-fi
-
 
 
 echo "Waiting for MariaDB..."
