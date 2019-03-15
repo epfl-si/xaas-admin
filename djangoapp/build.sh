7a40305219935747b3d31697ef81c97f0e9ad078
LuluTchab <l.chaboudez@gmail.com>
2019-03-15 13:16:42 +0100

    Improve script and add nicer display log

--- djangoapp/build.sh
+++ djangoapp/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo ""
+echo "###############################"
+echo "## CUSTOM BUILD SCRIPT START ##"
+echo "###############################"
+
 # Checking prerequisites
 if [ "${DJANGO_SETTINGS_MODULE}" == "" ]
 then
@@ -7,7 +12,12 @@ then
     exit 1
 fi
 
+
 #### Pip install ####
+echo ""
+echo "## PIP INSTALL ##"
+
+# Generating requirements file from environment setting.
 reqFile=`echo ${DJANGO_SETTINGS_MODULE} | awk -F. '{print $3".txt"}'`
 
 echo "Updating modules using pip (${reqFile})... "
@@ -17,30 +27,26 @@ pip install --no-cache-dir -r /tmp/${reqFile}
 
 
 #### Application source ####
-TMP_APP_SOURCE_FILES=/tmp/xaas-admin
-WITNESS_FILE=/tmp/source-done
 
-# If this is first execution of container,
-if [ ! -e ${WITNESS_FILE} ]
+echo ""
+echo "## SETTING APPLICATION SOURCE ##"
+# We set correct location for app source files
+echo "Setting correct source for Django app..."
+
+# If we have to use external source files for app (development purpose),
+if [ "${DJANGO_SETTINGS_MODULE}" = "config.settings.local" ]
 then
-    # We set correct location for app source files
-    echo "Setting correct source for Django app..."
+    echo "-> Using external source for app, deleting what was copied..."
+    rm -rf ${TMP_APP_SOURCE_FILES}
 
-    # If we have to use external source files for app (development purpose),
-    if [ "${DJANGO_SETTINGS_MODULE}" = "config.settings.local" ]
-    then
-        echo "-> Using external source for app, deleting what was copied..."
-        rm -rf ${TMP_APP_SOURCE_FILES}
+else # We have to use internal source (Test or production, typically on OpenShift)
 
-    else # We have to use internal source (Test or production, typically on OpenShift)
-
-        echo "-> Using internal source for app, copying to correct place..."
-        mv ${TMP_APP_SOURCE_FILES} /usr/src/
-
-    fi
-
-    # We add the witness file right after. In fact there's an error on OpenShift (permission denied) if
-    # we try to just move source files from one location to another... so we use witness file.
-    touch ${WITNESS_FILE}
+    echo "-> Using internal source for app, moving to correct place..."
+    mv ${TMP_APP_SOURCE_FILES} /usr/src/
 
 fi
+
+echo "##############################"
+echo "## CUSTOM BUILD SCRIPT DONE ##"
+echo "##############################"
+echo ""
