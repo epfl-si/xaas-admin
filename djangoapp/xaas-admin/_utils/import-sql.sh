3e07924bbcea558055dc3f4e2dd6794ae14635fb
LuluTchab <l.chaboudez@gmail.com>
2019-03-20 09:05:03 +0100

    Improve SQL import script

--- djangoapp/xaas-admin/_utils/import-sql.sh
+++ djangoapp/xaas-admin/_utils/import-sql.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Check parameter
+if [ "$1" == "" ]
+then
+    echo "Usage: $0 <pathToSQLFile>"
+    exit 1
+fi
 
+# Check if given file exists
+if [ ! -e $1 ]
+then
+    echo "Error! given file ($1) doesn't exists!"
+    exit 1
+fi
 
 echo -n "Importing... "
-mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_HOST} ${MYSQL_DATABASE} < $1
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_HOST} ${MYSQL_DATABASE} --port=${MYSQL_PORT} < $1
 echo "done"
 
-rm $1
+echo ""
+echo "Don't forget to remove '$1' file"
