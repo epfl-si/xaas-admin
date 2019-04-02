13366754c59b61fc45a7ce5e08cbf8cb16d780d3
LuluTchab <l.chaboudez@gmail.com>
2019-04-02 09:28:06 +0200

    Improve static files

--- djangoapp/build.sh
+++ djangoapp/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+mkdir -p /usr/src/xaas-admin/static/admin/css/
+mkdir -p /static/admin/css/
+
 echo "base1" > /usr/src/xaas-admin/static/base.css
 echo "base2" > /usr/src/xaas-admin/static/admin/base.css
 echo "base3" > /usr/src/xaas-admin/static/admin/css/base.css
 echo "base4" > /static/base.css
 echo "base5" > /static/admin/base.css
 echo "base6" > /static/admin/css/base.css
-
 
 
 echo ""
