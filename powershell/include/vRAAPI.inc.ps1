1143842ccde47207bd27c4755a9a4d2fe037651c
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2019-05-23 07:04:39 +0200

    Also use RESTAPI base class for vRAAPI class

--- powershell/include/vRAAPI.inc.ps1
+++ powershell/include/vRAAPI.inc.ps1
@@ -18,10 +18,9 @@
    0.1 - Version de base
 
 #>
-class vRAAPI
+class vRAAPI: RESTAPI
 {
 	hidden [string]$token
-	hidden [string]$server
 	hidden [string]$tenant
 	hidden [System.Collections.Hashtable]$headers
 
@@ -35,7 +34,7 @@ class vRAAPI
 		IN  : $password			-> Mot de passe
 
 	#>
-	vRAAPI([string] $server, [string] $tenant, [string] $userAtDomain, [string] $password)
+	vRAAPI([string] $server, [string] $tenant, [string] $userAtDomain, [string] $password) : base($server) # Ceci appelle le constructeur parent
 	{
 		$this.server = $server
 		$this.tenant = $tenant
@@ -64,119 +63,6 @@ class vRAAPI
 
 	}
 
-
-	<#
-		-------------------------------------------------------------------------------------
-		BUT : Charge un fichier JSON et renvoie le code.
-				Le fichier doit se trouver dans le dossier spécifié par $global:JSON_TEMPLATE_FOLDER
-
-		IN  : $file				-> Fichier JSON à charger
-		IN  : $valToReplace	-> (optionnel) Dictionnaaire avec en clef la chaine de caractères
-										à remplacer dans le code JSON (qui sera mise entre {{ et }} dans
-										le fichier JSON). 
-										En valeur, on peut trouver :
-										- Chaîne de caractères à mettre à la place de la clef
-										- Tableau avec:
-											[0] -> Chaîne de caractères à mettre à la place de la clef
-											[1] -> $true|$false pour dire s'il faut remplacer aussi ou pas
-													les "" qui entourent la clef qui est entre {{ }}.
-													On est en effet obligés de mettre "" autour sinon on
-													pète l'intégrité du JSON.
-
-		RET : Objet créé depuis le code JSON
-	#>
-	hidden [Object] loadJSON([string] $file, [System.Collections.IDictionary] $valToReplace)
-	{
-		# Chemin complet jusqu'au fichier à charger
-		$filepath = (Join-Path $global:JSON_TEMPLATE_FOLDER $file)
-
-		# Si le fichier n'existe pas
-		if(-not( Test-Path $filepath))
-		{
-			Throw ("JSON file not found ({0})" -f $filepath)
-		}
-
-		# Chargement du code JSON
-		$json = (Get-Content -Path $filepath) -join "`n"
-
-		# S'il y a des valeurs à remplacer
-		if($null -ne $valToReplace)
-		{
-			# Parcours des remplacements à faire
-			foreach($search in $valToReplace.Keys)
-			{
-				# Si on a des infos pour savoir si on doit supprimer ou pas les doubles quotes 
-				if($valToReplace.Item($search) -is [Array])
-				{
-					# Extraction des informations 
-					$replaceWith, $removeDoubleQuotes = $valToReplace.Item($search)	
-				}
-				else # On a juste la chaîne de caractères 
-				{
-					$replaceWith = $valToReplace.Item($search)
-					$removeDoubleQuotes = $false
-				}
-
-				$search = "{{$($search)}}"
-				
-				# Si on doit supprimer les doubles quotes autour de {{ }}
-				if($removeDoubleQuotes)
-				{
-					# Ajout des doubles quotes pour la recherche
-					$search = "`"$($search)`""
-				}
-
-				# Recherche et remplacement de l'élément
-				$json = $json -replace $search, $replaceWith
-			}
-		}
-		try
-		{
-			return $json | ConvertFrom-Json
-		}
-		catch
-		{
-			Throw ("Error converting JSON from file ({0})" -f $filepath)
-		}
-	}
-
-
-	<#
-		-------------------------------------------------------------------------------------
-		BUT : Effectue un appel à l'API REST
-
-		IN  : $uri		-> URL à appeler
-		IN  : $method	-> Méthode à utiliser (Post, Get, Put, Delete)
-		IN  : $json 	-> Code JSON
-
-		RET : Retour de l'appel
-	#>
-	hidden [Object] callAPI([string]$uri, [string]$method, [string]$json)
-	{
-		try
-		{
-			if($json -ne "")
-			{
-				return Invoke-RestMethod -Uri $uri -Method $method -Headers $this.headers -Body $json
-			}
-			else 
-			{
-				return Invoke-RestMethod -Uri $uri -Method $method -Headers $this.headers
-			}
-		}
-		catch 
-		{
-			# Si une erreur survient, on la "repropage" mais avec un message d'erreur plus parlant qu'un "Bad Request" ou autre... 
-			# On va récupérer le message qui a été renvoyé par vRA et on va le rebalance en exception !
-			$errorDetails = ConvertFrom-Json $_.ErrorDetails.Message
-
-			# On récupère aussi le nom de la fonction qui a appelé celle-ci, histoire d'avoir un peu d'infos dans le message d'erreur
-			$callStack = Get-PSCallStack
-			$parentFunc = $callStack[1].FunctionName
-
-			Throw ("VRAAPI::{0}(): {1}" -f $parentFunc, $errorDetails.errors.message)
-		}
-	}
 
 	<#
 		-------------------------------------------------------------------------------------
