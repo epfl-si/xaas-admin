ed9fa1e992045a1f3557f8d65923cab5cd7a28ea
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2019-07-19 08:49:21 +0200

    Remove comment

--- powershell/xaas-backup-simple.ps1
+++ powershell/xaas-backup-simple.ps1
@@ -28,12 +28,6 @@ $dummy = Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$fa
 # On encrypte le mot de passe
 $credSecurePwd = $global:XAAS_BACKUP_VCENTER_PASSWORD_LIST[$targetEnv] | ConvertTo-SecureString -AsPlainText -Force
 $credObject = New-Object System.Management.Automation.PSCredential -ArgumentList $global:XAAS_BACKUP_VCENTER_USER_LIST[$targetEnv], $credSecurePwd
-
-# Connexion au serveur vSphere.
-# On passe par le paramètre -credential car sinon, on a des erreurs avec Get-AssignmentTag et Get-Tag, ou peut-être tout simplement avec les commandes dans
-# lesquelles on faite un | pour passer à la suivante.
 $connectedvCenter = Connect-VIServer -Server $global:XAAS_BACKUP_VCENTER_SERVER_LIST[$targetEnv] -credential $credObject
-
-
 $vm = Get-VM -Server $vSphere -Name $vmName
 $tagList = Get-TagAssignment -Server $vSphere -Entity $vm
