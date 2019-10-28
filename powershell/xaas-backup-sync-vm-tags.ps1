57eb96bbee39a145052df774aadceeadb68e9c64
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2019-10-28 15:40:54 +0100

    Bugfix

--- powershell/xaas-backup-sync-vm-tags.ps1
+++ powershell/xaas-backup-sync-vm-tags.ps1
@@ -200,7 +200,7 @@ catch
     $errorMessage = $errorMessage -replace "`n", "<br>"
 
 	# Envoi d'un message d'erreur aux admins 
-	$mailSubject = getvRAMailSubject -shortSubject ("Error in script '{0}'" -f $MyInvocation.MyCommand.Name) -targetEnv $targetEnv -targetTenant ""
+	$mailSubject = getvRAMailSubject -shortSubject ("Error in script '{0}'" -f $MyInvocation.MyCommand.Name) -targetEnv $targetEnv -targetTenant $targetTenant
 	$mailMessage = getvRAMailContent -content ("<b>Computer:</b> {3}<br><b>Script:</b> {0}<br><b>Parameters:</b>{4}<br><b>Error:</b> {1}<br><b>Trace:</b> <pre>{2}</pre>" -f `
 	$MyInvocation.MyCommand.Name, $errorMessage, [System.Net.WebUtility]::HtmlEncode($errorTrace), $env:computername, (formatParameters -parameters $PsBoundParameters))
 
