12f4db07de33460016cb6d144f4aa33852f20777
Eric-Krejci-EPFL <eric.krejci@epfl.ch>
2020-01-21 08:55:10 +0100

    Adapt to answer 'locked CD-ROM' question

--- powershell/clean-iso-folders.ps1
+++ powershell/clean-iso-folders.ps1
@@ -47,6 +47,51 @@ $configGlobal = [ConfigReader]::New("config-global.json")
 
 
 
+# --------------------------------------------------------------------------------
+# --------------------------------- FONCTIONS ------------------------------------
+
+# Fonction trouvée ici: http://www.lucd.info/2015/10/02/answer-the-question/
+# Quelques adaptations ont été faite
+function Set-CDDriveAndAnswer
+{
+  [CmdletBinding()]
+  param(
+  [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+  [PSObject]$vm
+  )
+ 
+  Begin
+  {
+    $cdQuestion = {
+      param($vm)
+    
+      $maxPass = 10
+      $pass = 0
+    
+      while($pass -lt $maxPass){
+		$question = $vm | Get-VMQuestion -QuestionText "*locked the CD-ROM*"
+		
+        if($question){
+          Set-VMQuestion -VMQuestion $question -Option button.yes -Confirm:$false
+          $pass = $maxPass + 1
+        }
+        $pass++
+      }
+      Start-Sleep 2
+    }
+  }
+ 
+  Process
+  {
+    
+      $cd = Get-CDDrive -VM $vm
+      $job = Start-Job -Name Check-CDQuestion -ScriptBlock $cdQuestion -ArgumentList $vm
+      Set-CDDrive -CD $cd -NoMedia -Confirm:$false -ErrorAction Stop
+    
+  }
+}
+
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
@@ -98,7 +143,7 @@ try
 		# Recherche du nom du BG auquel l'ISO est associée 
 		$bgName = $nameGenerator.getNASPrivateISOPathBGName($isoFile.FullName)
 		
-		$logHistory.addLineAndDisplay(("ISO file found! ${0} - BG:${1}" -f $isoFile.FullName, $bgName))
+		$logHistory.addLineAndDisplay(("ISO file found! {0} - BG: {1}" -f $isoFile.FullName, $bgName))
 		$counters.inc('ISOFound')
 
 		# Si on n'a pas encore de connexion à vRA, là, ça serait bien d'en ouvrir une histoire pouvoir continuer.
@@ -137,13 +182,15 @@ try
 			# Recherche de la liste des VM existantes dans le Tenant
 			$vmList = $vra.getBGItemList($bg, 'Virtual Machine')
 
+			$logHistory.addLineAndDisplay( ("Looking for VM in BG {0} to see if ISO file is mounted" -f $bgName) )
 			# Parcours des VM
 			ForEach($vm in $vmList)
 			{
-				$logHistory.addLineAndDisplay(("Checking VM '${0}'..." -f $vm.name))
+				$logHistory.addLineAndDisplay(("Checking VM '{0}'..." -f $vm.name))
 
 				# On recherche une éventuelle ISO montée 
 				$mountedISO = (Get-VM -Name $vm.name | Get-CDDrive).IsoPath
+
 
 				# Si c'est différent de $null, ça ne veut pas encore dire qu'il y a une ISO de montée
 				if($null -ne $mountedISO)
@@ -155,16 +202,19 @@ try
 					# Dans ce dernier cas, c'est comme si aucune ISO n'était montée.
 					
 					# On commencer par virer le nom du datastore (même s'il est vide) et on dit merci aux expressions régulières
-					$mountedISO = ($mountedISO -replace "\[.*\]\s", "").Trim()
+					# On transforme aussi les / en \ parce que notre chemin jusqu'à l'ISO version Windows contient des \ alors que sur
+					# vSphere, ce sont des / qui sont utilisés... et si on ne remplace pas, on ne pourra pas comparer correctement
+					# les path juste après...
+					$mountedISO = (($mountedISO -replace "\[.*\]\s", "") -replace "/", "\").Trim()
 
 					# On regarde maintenant si le chemin jusqu'à l'ISO que l'on a trouvée sur le volume se termine par celui de l'ISO montée
 					# Si c'est le cas, c'est que l'ISO est utilisée donc on ne peut pas l'effacer 
 					if($mountedISO -ne "" -and $isoFile.FullName.endsWith($mountedISO))
 					{
-						# on démonte l'image en mettant "no-media" 
-						Get-VM -Name $vm.name | Get-CDDrive | Set-CDDrive -NoMedia -Confirm:$false
-
+						
 						$logHistory.addLineAndDisplay("ISO file is mounted in VM. Unmounting...")
+						# on démonte l'image en mettant "no-media" 
+						Get-VM -Name $vm.name | Set-CDDriveAndAnswer
 						$counters.inc('ISOUnmounted')
 					}
 					
@@ -175,14 +225,14 @@ try
 		} 
 		else # Le BG n'existe pas 
 		{
-			$logHistory.addLineAndDisplay(("Business Group not found in vRA '${0}'" -f $bgName))
+			$logHistory.addLineAndDisplay(("Business Group not found in vRA '{0}'" -f $bgName))
 		}
 		
 		# Quand on arrive ici, on assure que l'ISO n'est plus utilisée par aucune VM si c'était le cas auparavant avant.
 		# On peut donc la faire disparaître
 
 		Remove-Item $isoFile.FullName -Force
-		$logHistory.addLineAndDisplay(("ISO file deleted: ${0}" -f $isoFile.FullName))
+		$logHistory.addLineAndDisplay(("ISO file deleted: {0}" -f $isoFile.FullName))
 		$counters.inc('ISODeleted')
 		
 
