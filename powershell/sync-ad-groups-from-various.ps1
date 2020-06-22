86f7d2c49495b4f75218756bcc566768b80224e4
Eric-Krejci-EPFL <eric.krejci@epfl.ch>
2020-06-22 14:43:22 +0200

    Quickfixes

--- powershell/sync-ad-groups-from-various.ps1
+++ powershell/sync-ad-groups-from-various.ps1
@@ -1,6 +1,6 @@
 <#
 USAGES:
-	sync-ad-groups-from-ldap-or-django.ps1 -targetEnv prod|test|dev -targetTenant vsphere.local|itservices|epfl
+	sync-ad-groups-from-various.ps1 -targetEnv prod|test|dev -targetTenant vsphere.local|itservices|epfl
 #>
 <#
 	BUT 		: Crée/met à jour les groupes AD pour l'environnement donné et le tenant EPFL.
