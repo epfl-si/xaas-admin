584a19ff39584a1efc64c8bc25f92cb675d69b51
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2020-09-28 13:00:42 +0200

    Doesn't use PowerShell connection anymore, everything is done using REST

--- powershell/mynas-process-waiting-quota-update-requests.ps1
+++ powershell/mynas-process-waiting-quota-update-requests.ps1
@@ -31,11 +31,18 @@
 . ([IO.Path]::Combine("$PSScriptRoot", "include", "functions.inc.ps1"))
 . ([IO.Path]::Combine("$PSScriptRoot", "include", "LogHistory.inc.ps1"))
 . ([IO.Path]::Combine("$PSScriptRoot", "include", "ConfigReader.inc.ps1"))
+. ([IO.Path]::Combine("$PSScriptRoot", "include", "Counters.inc.ps1"))
 . ([IO.Path]::Combine("$PSScriptRoot", "include", "NotificationMail.inc.ps1"))
 
 . ([IO.Path]::Combine("$PSScriptRoot", "include", "MyNAS", "define.inc.ps1"))
 . ([IO.Path]::Combine("$PSScriptRoot", "include", "MyNAS", "func-netapp.inc.ps1"))
 . ([IO.Path]::Combine("$PSScriptRoot", "include", "MyNAS", "func.inc.ps1"))
+
+. ([IO.Path]::Combine("$PSScriptRoot", "include", "REST", "APIUtils.inc.ps1"))
+. ([IO.Path]::Combine("$PSScriptRoot", "include", "REST", "RESTAPI.inc.ps1"))
+. ([IO.Path]::Combine("$PSScriptRoot", "include", "REST", "RESTAPICurl.inc.ps1"))
+
+. ([IO.Path]::Combine("$PSScriptRoot", "include", "REST", "XaaS", "NAS", "NetAppAPI.inc.ps1"))
 
 # Chargement des fichiers de configuration
 $configMyNAS = [ConfigReader]::New("config-mynas.json")
@@ -78,30 +85,25 @@ try
    # Objet pour pouvoir envoyer des mails de notification
    $notificationMail = [NotificationMail]::new($configGlobal.getConfigValue("mail", "admin"), $global:MAIL_TEMPLATE_FOLDER, "MyNAS", "")
    
-   # Chargement du module (si nécessaire)
-   loadDataOnTapModule 
+   $netapp = $null
 
+   # Parcours des serveurs qui sont définis
+   $configMyNAS.getConfigValue("nas", "serverList") | ForEach-Object {
 
-   try 
-   {
-      $logHistory.addLineAndDisplay("Connecting... ")
-      # Génération du mot de passe 
-      $secPassword = ConvertTo-SecureString $configMyNAS.getConfigValue("nas", "password") -AsPlainText -Force
-      # Création des credentials pour l'utilisateur
-      $credentials = New-Object System.Management.Automation.PSCredential($configMyNAS.getConfigValue("nas", "user"), $secPassword)
-      
-      # Connexion au NetApp
-      $connectHandle = Connect-NcController -Name $global:CLUSTER_COLL_IP -Credential $credentials -HTTPS
+      if($null -eq $netapp)
+      {
+         # Création de l'objet pour communiquer avec le NAS
+         $netapp = [NetAppAPI]::new($_, $configMyNAS.getConfigValue("nas", "user"), $configMyNAS.getConfigValue("nas", "password"))
+      }
+      else
+      {
+         $netapp.addTargetServer($_)
+      }
+   }# Fin boucle de parcours des serveurs qui sont définis
 
-   }
-   catch
-   {
-      Throw "Error connecting to "+$global:CLUSTER_COLL_IP+"!"
-   }
-
+   # le format des lignes renvoyées est le suivant :
+   # <volumeName>,<usernameShort>,<vServerName>,<Sciper>,<softQuotaKB>,<hardQuotaKB>
    $quotaUpdateList = getWebPageLines -url ("$global:WEBSITE_URL_MYNAS/ws/get-quota-updates.php?fs_mig_type=mig")
-
-   #$quotaUpdateList = @("dit_filesx_indiv,svonsieb,filesx,123456,4194304,4194304")
 
    if($quotaUpdateList -eq $false)
    {
@@ -120,133 +122,70 @@ try
       exit 0
    }
 
-   # le format des lignes renvoyées est le suivant :
-   # <volumeName>,<usernameShort>,<vServerName>,<Sciper>,<softQuotaKB>,<hardQuotaKB>
-
-
-
-   # La liste des utilisateurs par vServer
-   $userPervServer=@{}
-   # Nouveau quotas des utilisateurs 
-   $userNewQuotaMessage=@{}
-
    $doneMailMessage="Users updated:<br><table border='1' style='border-collapse:collapse;padding:3px;'><tr><td><b>Username</b></td><td><b>Old quota [MB]</b></td><td><b>New quota [MB]</b></td></tr>"
 
    $oneQuotaUpdateDone=$false
+
+   # Pour la liste des volumes
+   $volList = @{}
 
    # Parcours des éléments à renommer 
    foreach($updateInfos in $quotaUpdateList)
    {
       $quotaInfosArray = $updateInfos.split(',')
 
-      # Génréation des informations 
-      $volumeName=$quotaInfosArray[0]
-      $username = $quotaInfosArray[1]
-      $usernameAndDomain="INTRANET\"+$quotaInfosArray[1]
-      $vserverName =$quotaInfosArray[2]
-      $sciper = $quotaInfosArray[3]
-      # Changemenet de la manière de "set" le quota depuis PowerShell Toolkit 4.4. Il faut maintenant passer des bytes au lieu 
-      # d'un chiffre suivi de l'unité (ex: 25GB)
-      $softKB=([int]$quotaInfosArray[4])*1024
-      $hardKB=([int]$quotaInfosArray[5])*1024   
-   #   $softKB=$quotaInfosArray[4]+"KB"
-   #   $hardKB=$quotaInfosArray[5]+"KB"
-      # Extraction du no du vServer 
-      $vserverNo = $vserverName.Substring($vserverName.Length-1)
+      # Extraction des infos
+      $volumeName, $username, $vserverName, $sciper, $softKB, $hardKB = $updateInfos.split(',')
 
-      # Recherche des infos du vServer sur lequel se trouve l'entrée de quota à modifier 
-      $vserver = Get-NcVserver -Controller $connectHandle -Name $vserverName
-      
+      # Génréation des informations 
+      $usernameAndDomain="INTRANET\"+$username
+   
       
       $logHistory.addLineAndDisplay("Changing quota for $usernameAndDomain ")
       
+      # Si on n'a pas encore les infos du volume en cache,
+      if($volList.Keys -notcontains $volumeName)
+      {
+         # Recherche des infos du volume sur lequel on doit travailler
+         $volList.$volumeName = $netapp.getVolumeByName($volumeName)
+      }
+
       # Recherche du quota actuel 
-      $currentQuota = Get-NcQuotaReport -Controller $connectHandle -Vserver $vserver -Volume $volumeName -Target $usernameAndDomain
+      $currentQuota = $netapp.getUserQuotaRule($volList.$volumeName, $usernameAndDomain)
       
       # Si pas trouvé, c'est que l'utilisateur a le quota par défaut
       if($null -eq $currentQuota)
       {
-         $currentQuota = "'default'"
+         $currentQuotaMB = "'default'"
       }
       else
       {
-         $currentQuota = ([Math]::Floor($currentQuota.DiskLimit/1024))
+         $currentQuotaMB = ([Math]::Floor($currentQuota.space.hard_limit/1024/1024))
       }
       
-      $logHistory.addLineAndDisplay(("-> Current: "+$currentQuota+" MB - New: "+([Math]::Floor($quotaInfosArray[5]/1024))+" MB... ") )
-      # Exécution de la requête 
-      $res = Set-NcQuota -Controller $connectHandle -VserverContext $vserver -Volume $volumeName -Target $usernameAndDomain -Type user -Qtree "" -DiskLimit $hardKB -SoftDiskLimit $softKB
-
-      # Si on n'a pas encore traité ce no de volume, 
-      if($userPervServer.Keys -notcontains $vserverNo)
+      # Si le quota est différent ou que l'entrée de quota n'existe pas,
+      if(($null -eq $currentQuota) -or ($currentQuota.space.hard_limit -ne ($hardKB * 1024)))
       {
-         # Création d'une liste pour le vServer courant et ajout 
-         $userPervServer.$vserverNo = @()
+         $logHistory.addLineAndDisplay(("-> Updating quota... Current: "+$currentQuotaMB+" MB - New: "+([Math]::Floor($hardKB/1024))+" MB... ") )
+         
+         # Exécution de la requête (et attente que le resize soit fait)
+         $netapp.updateUserQuotaRule($volList.$volumeName, $usernameAndDomain, $hardKB/1024)
+         
+         # On initialise la requête comme ayant été traitée
+         setQuotaUpdateDone -userSciper $sciper
+            
+         # Ajout de l'info au message qu'on aura dans le mail 
+         $doneMailMessage += ([string]::Concat("<tr><td>",$username ,"</td><td>", $currentQuota, "</td><td>", ([Math]::Floor($quotaInfosArray[5]/1024)), "</td></tr>"))
+         
+         $oneQuotaUpdateDone=$true
+
       }
-      
-      # Ajout du Sciper de l'utilisateur dans la liste 
-      $userPervServer.$vserverNo += $sciper
-      
-      # Enregistrement du nouveau quota pour l'utilisateur
-      $userNewQuotaMessage.$sciper = ([string]::Concat("<tr><td>",$username ,"</td><td>", $currentQuota, "</td><td>", ([Math]::Floor($quotaInfosArray[5]/1024)), "</td></tr>"))
-      
+      else # Le quota est correct
+      {
+         $logHistory.addLineAndDisplay(( "-> Quota is correct ({0} MB), no change needed" -f $currentQuota ))
+      }
 
    }# FIN BOUCLE de parcours des quotas à modifier
-
-   <# 
-      Une fois que toutes les modifications de quota on été effectuées sur les X volumes concernés,
-      on peut faire les "resize" sur les volumes en question. Et ce n'est que quand un "resize" se 
-      termine correctement que l'on initialise les utilisateurs du volume concerné comme ayant leur
-      demande d'augmentation de quota traitée. Et c'est aussi à ce moment-là qu'on les ajoute dans
-      le message qui sera envoyé par mail à la fin de l'exécution du script
-   #>
-
-
-
-   # Parcours des no de vServer sur lequels des extensions de quota ont été effectuées
-   foreach($vserverNo in $userPervServer.Keys)
-   {
-      $volumeName = "dit_files"+$vserverNo+"_indiv"
-      $vserverName = "files"+$vserverNo
-      
-      # Recherche des infos du vServer sur lequel on doit faire le resize
-      $vserver = Get-NcVserver -Controller $connectHandle -Name $vserverName
-      
-      $logHistory.addLineAndDisplay("Resizing volume $volumeName... ")
-      # Démarrage du Resize pour le volume 
-      $res = resizeQuota -controller $connectHandle -onVServer $vserver -volumeName $volumeName
-      
-      # Si erreur dans le traitement 
-      if($res.JobState -eq "failure")
-      {
-         # Envoi d'un mail aux admins 
-         sendMailToAdmins -mailMessage ([string]::Concat("Error doing 'quota resize' on volume $volumeName<br><b>Error:</b><br>", $res.JobCompletion, `
-               "<br>There is several possibilities:<ul>", `
-               "<li>There is probably some users delete requests that not have been processed. Process them and wait until the next quota update process.</li>", `
-               "<li>It is possible that the user will be renamed tomorrow. The account is probably already renamed in AD and the folder has to be renamed.</li>", `
-               "</ul>")) `
-               -mailSubject "MyNAS Service: Error processing quota update requests for volume $volumeName"   
-                              
-         Write-Host "Error doing resize, skipping this volume..."
-         
-      }
-      else # Pas d'erreur dans le traitement 
-      {
-         $logHistory.addLineAndDisplay("Setting update as done for users on volume $volumeName... ")
-         # Parcours des Sciper se trouvant sur le volume que l'on vient de "resize"
-         foreach($sciper in $userPervServer.$vserverNo)
-         {
-            # On initialise la requête comme ayant été traitée
-            setQuotaUpdateDone -userSciper $sciper
-            
-            # Ajout de l'info au message qu'on aura dans le mail 
-            $doneMailMessage += $userNewQuotaMessage.$sciper
-            
-            $oneQuotaUpdateDone=$true
-         }
-      }# FIN SI pas d'erreur dans le traitement
-      
-   }# FIN BOUCLE DE parcours de vServer
 
 
    # Si on a fait au moins une extension de quota
