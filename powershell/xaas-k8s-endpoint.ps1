4193be2468b640fd44938b88d85ded817d611d78
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2021-02-08 17:15:37 +0100

    Use global to point to root log directory

--- powershell/xaas-k8s-endpoint.ps1
+++ powershell/xaas-k8s-endpoint.ps1
@@ -507,7 +507,7 @@ try
     $output = getObjectForOutput
 
     # Création de l'objet pour logguer les exécutions du script (celui-ci sera accédé en variable globale même si c'est pas propre XD)
-    $logHistory = [LogHistory]::new('xaas-k8s', (Join-Path $PSScriptRoot "logs"), 30)
+    $logHistory = [LogHistory]::new('xaas-k8s', $global:LOGS_FOLDER, 30)
     
     # Objet pour pouvoir envoyer des mails de notification
     $valToReplace = @{
