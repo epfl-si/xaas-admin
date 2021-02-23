534d9ce32790e9306d1df9bed15a6796505b5d84
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2021-02-23 15:21:22 +0100

    Remove func

--- powershell/xaas-k8s-endpoint.ps1
+++ powershell/xaas-k8s-endpoint.ps1
@@ -379,48 +379,6 @@ function configureNamespaceElements([string]$clusterName, [string]$namespace, [s
 
 <#
     -------------------------------------------------------------------------------------
-    BUT : Renvoie les groupes d'accès à utiliser pour donner les droits sur un cluster
-
-    IN  : $vra          -> Objet permettant d'accéder à vRA
-    IN  : $bg           -> Objet représentant le Business Group auquel le cluster est lié
-    IN  : $targetTenant -> Tenant sur lequel se trouve le BusinessGroup
-
-    RET : Tableau avec la liste des groupes d'accès à utiliser
-        $null si pas trouvé
-#>
-function getBGAccessGroupList([vRAAPI]$vra, [PSObject]$bg, [string]$targetTenant)
-{
-    if($null -eq $bg)
-    {
-        Throw "Business Group cannot be NULL"
-    }
-    <# On explose l'infos <group>@intranet.epfl.ch pour n'extraire que le nom du groupe
-        Récupération des utilisateurs qui ont le droit de demander des cluster, ça sera ceux
-        qui pourront gérer le cluster #>
-    $userAndGroupList = $vra.getBGRoleContent($bg.id, "CSP_CONSUMER")
-    $groupName, $null = $userAndGroupList[0] -split '@'
-
-    # Si on est dans le tenant ITS
-    if($targetTenant -eq $global:VRA_TENANT__ITSERVICES)
-    {
-        # Récupération de la liste des groupes qui sont présents dans le 1er groupe qui est dans les CONSUMER du BusinessGroup
-        # NOTE: On fait ceci car TKGI/Harbor ne gèrent pas les groupes nested... 
-        # FIXME: A supprimer une fois que TKGI/Harbor pourra gérer le groupes nested
-        $accessGroupList = Get-ADGroupMember $groupName | Where-Object { $_.objectClass -eq "group"} | Select-Object -ExpandProperty name
-
-    }
-    else # Autres tenants
-    {
-        # On prent le groupe tel quel
-        $accessGroupList = @($groupName)
-    }
-
-    return $accessGroupList
-}
-
-
-<#
-    -------------------------------------------------------------------------------------
     BUT : Renvoie la liste des namespaces d'un cluster
 
     IN  : $clusterName      -> Nom du cluster
