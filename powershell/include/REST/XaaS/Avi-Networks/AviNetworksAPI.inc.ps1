d05fe3dbf66b29f6f54893d666a3d56540e82044
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2021-02-23 10:17:43 +0100

    First files

---
+++ powershell/include/REST/XaaS/Avi-Networks/AviNetworksAPI.inc.ps1
@@ -0,0 +1,179 @@
+<#
+   	BUT : Contient les fonctions donnant accès à l'API Avi Networks.
+		
+   	AUTEUR : Lucien Chaboudez
+   	DATE   : Février 2021
+
+	Documentation:
+		- API: https://vsissp-avi-ctrl-t.epfl.ch/swagger/
+		
+
+	REMARQUES :
+	- Cette classe hérite de RESTAPICurl. Pourquoi Curl? parce que si on utilise le CmdLet
+		Invoke-RestMethod, on a une erreur de connexion refermée, et c'est la même chose 
+		lorsque l'on veut parler à l'API de NSX-T. C'est pour cette raison que l'on passe
+		par Curl par derrière.
+
+
+#>
+
+$global:XAAS_AVI_NETWORK_API_VERSION = "20.1.4"
+
+class AviNetworksAPI: RESTAPICurl
+{
+	hidden [System.Collections.Hashtable]$headers
+    
+
+	<#
+	-------------------------------------------------------------------------------------
+		BUT : Créer une instance de l'objet et ouvre une connexion au serveur
+
+		IN  : $server			-> Nom DNS du serveur
+		IN  : $username	        -> Nom d'utilisateur
+		IN  : $password			-> Mot de passe
+
+	#>
+	AviNetworksAPI([string] $server, [string] $username, [string] $password) : base($server) # Ceci appelle le constructeur parent
+	{
+		$this.headers = @{}
+		$this.headers.Add('Accept', 'application/json')
+		$this.headers.Add('Content-Type', 'application/json')
+        $this.headers.Add('X-Avi-Version', $global:XAAS_AVI_NETWORK_API_VERSION)
+        $this.headers.Add('X-Avi-Tenant', 'admin')
+
+		$authInfos = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+
+		# Mise à jour des headers
+		$this.headers.Add('Authorization', ("Basic {0}" -f $authInfos))
+
+		$this.baseUrl = "{0}/api" -f $this.baseUrl
+
+		# Pour autoriser les certificats self-signed
+		[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $True }
+
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    }    
+
+
+    <#
+		-------------------------------------------------------------------------------------
+		BUT : Effectue un appel à l'API REST via Curl
+
+		IN  : $uri		-> URL à appeler
+		IN  : $method	-> Méthode à utiliser (Post, Get, Put, Delete)
+		IN  : $body 	-> Objet à passer en Body de la requête. On va ensuite le transformer en JSON
+						 	Si $null, on ne passe rien.
+
+		RET : Retour de l'appel
+	#>
+	hidden [Object] callAPI([string]$uri, [string]$method, [System.Object]$body)
+	{
+		# On fait un "cast" pour être sûr d'appeler la fonction de la classe courante et pas une surcharge éventuelle
+		$result = ([RESTAPICurl]$this).callAPI($uri, $method, $body)
+
+        if(objectPropertyExists -obj $result -propertyName "error")
+        {
+            Throw $result.error
+        }
+
+        return $result
+	}
+
+    <# --------------------------------------------------------------------------------------------------------- 
+                                                    TENANTS
+       --------------------------------------------------------------------------------------------------------- #>
+
+    
+    <#
+	-------------------------------------------------------------------------------------
+		BUT : Renvoie la liste de tenants existants
+
+        RET : Tableau avec la liste des tenants
+	#>
+    [Array] getTenantList()
+    {
+        $uri = "{0}/tenant" -f $this.baseUrl
+
+        return ($this.callAPI($uri, "Get", $null)).results
+    }
+
+
+    <#
+	-------------------------------------------------------------------------------------
+		BUT : Ajoute un tenant
+
+        IN  : $name         -> Nom du tenant
+        IN  : $description  -> Description du tenant
+
+        RET : Objet représentant le tenant
+	#>
+    [PSObject] addTenant([string]$name, [string]$description)
+    {
+        $uri = "{0}/tenant" -f $this.baseUrl
+
+        $replace = @{
+			name = $name
+			description = $description
+		}
+
+		$body = $this.createObjectFromJSON("xaas-avi-networks-new-tenant.json", $replace)
+
+        return $this.callAPI($uri, "POST", $body) 
+
+    }
+
+
+    <#
+	-------------------------------------------------------------------------------------
+		BUT : Renvoie un tenant par son ID
+
+        IN  : $id       -> ID du tenant
+
+        RET : Objet représentant le tenant
+                Exception si le tenant n'existe pas
+	#>
+    [PSObject] getTenantById([string]$id)
+    {
+        $uri = "{0}/tenant/{1}" -f $this.baseUrl, $id
+
+        return $this.callAPI($uri, "GET", $null)
+    }
+
+
+    <#
+	-------------------------------------------------------------------------------------
+		BUT : Renvoie un tenant par son nom
+
+        IN  : $nom       -> nom du tenant
+
+        RET : Objet représentant le tenant
+                $null si pas trouvé
+	#>
+    [PSObject] getTenantByName([string]$name)
+    {
+        $uri = "{0}/tenant?name={1}" -f $this.baseUrl, $name
+
+        $res = $this.callAPI($uri, "GET", $null).results
+
+        if($res.count -eq 0)
+        {
+            return $null
+        }
+        return $res[0]
+    }
+
+
+    <#
+	-------------------------------------------------------------------------------------
+		BUT : Efface un tenant
+
+        IN  : $id       -> ID du tenant
+	#>
+    [void] deleteTenant([string]$id)
+    {
+        $uri = "{0}/tenant/{1}" -f $this.baseUrl, $id
+
+        $this.callAPI($uri, "DELETE", $null) | Out-Null
+    }
+}
---
+++ powershell/include/XaaS/Avi-Networks/NameGeneratorAviNetworks.inc.ps1
@@ -0,0 +1,57 @@
+<#
+   BUT : Classe permettant de donner des informations sur les noms à utiliser pour tout ce qui est XaaS AVI Network
+
+   AUTEUR : Lucien Chaboudez
+   DATE   : Février 2021
+
+#>
+
+enum AviNetworksTenantType
+{
+   Development
+   Test
+   Production
+}
+
+class NameGeneratorAviNetworks: NameGeneratorBase
+{
+   
+   <#
+		-------------------------------------------------------------------------------------
+		BUT : Constructeur de classe.
+
+        IN  : $vraEnv       -> Environnement sur lequel on travaille, notion vRA du terme
+                                $TARGET_ENV_DEV
+                                $TARGET_ENV_TEST
+                                $TARGET_ENV_PROD
+        IN  : $vraTenant   -> Tenant sur lequel on travaille
+                                $VRA_TENANT_DEFAULT
+                                $VRA_TENANT_EPFL
+                                $VRA_TENANT_ITSERVICES
+
+		RET : Instance de l'objet
+	#>
+    NameGeneratorAviNetworks([string]$vraEnv, [string]$vraTenant): base($vraEnv, $vraTenant)
+   {
+   }
+
+
+   <#
+		-------------------------------------------------------------------------------------
+		BUT : Renvoie le nom et la description d'un tenant pour un BG donné
+
+        IN  : $bgName       -> Nom du BusinessGroup
+        IN  : $tenantType   -> Type de tenant (voir en haut du présent fichier)
+
+		RET : Tableau avec
+                - Nom du tenant
+                - Description du tenant
+	#>
+    [Array] getTenantNameAndDesc([string]$bgName, [AviNetworksTenantType]$tenantType)
+    {
+        return @(
+            ("{0}-{1}" -f $bgName, $tenantType.ToString()),
+            ("BG: {0}, Type: {1}" -f $bgName, $tenantType.ToString())
+        )
+    }
+}
