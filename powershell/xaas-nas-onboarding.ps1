0027ed5a4ec3533be8bdeac3e3aec8663384c937
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2021-03-12 07:47:01 +0100

    Continue dev

--- powershell/xaas-nas-onboarding.ps1
+++ powershell/xaas-nas-onboarding.ps1
@@ -1,7 +1,7 @@
 <#
 USAGES:
-    xaas-nas-onboarding.ps1 -targetEnv prod|test|dev -targetTenant epfl|research -action genDataFile -dataFile <dataFile>
-    xaas-nas-onboarding.ps1 -targetEnv prod|test|dev -targetTenant epfl|research -action import -dataFile <dataFile>
+    xaas-nas-onboarding.ps1 -targetEnv prod|test|dev -targetTenant epfl|research -action genDataFile -volType col|app -dataFile <dataFile>
+    xaas-nas-onboarding.ps1 -targetEnv prod|test|dev -targetTenant epfl|research -action import -volType col|app -dataFile <dataFile>
 #>
 <#
     BUT 		: Script permettant d'effectuer le onboarding des volumes NAS
@@ -14,6 +14,7 @@ USAGES:
 param([string]$targetEnv, 
       [string]$targetTenant,
       [string]$action,
+      [string]$volType,
       [string]$dataFile)
 
 
@@ -45,6 +46,7 @@ param([string]$targetEnv,
 
 # Chargement des fichiers de configuration
 $configGlobal = [ConfigReader]::New("config-global.json")
+$configvRA = [ConfigReader]::New("config-vra.json")
 $configNAS = [ConfigReader]::New("config-xaas-nas.json")
 
 
@@ -55,35 +57,51 @@ $configNAS = [ConfigReader]::New("config-xaas-nas.json")
 $ACTION_GEN_DATA_FILE       = "genDataFile"
 $ACTION_IMPORT              = "import"
 
+$CSV_SEPARATOR = ";"
+
+$XAAS_NAS_ONBOARD_COLL_CATALOG_ITEM =  "NAS(C) Onboard"
+
 
 # -------------------------------------------- FONCTIONS ---------------------------------------------------
 
-function getVolToOnboard([SQLDB]$sqldb)
+function getVolToOnboard([SQLDB]$sqldb, [string]$volType)
 {
+
+    $volTypeIdList = switch($volType) 
+    {
+        "col"{ @(2,4,5)}
+        "app"{ @(1) }
+    }
+
     $tables = @(
         "nas_fs",
         "infra_vserver",
-        "nas_service_type"
+        "nas_service_type",
+        "nas_fs_admin_mail"
     )
 
     $conditions = @(
         "nas_fs.infra_vserver_id = infra_vserver.infra_vserver_id",
         "nas_fs.nas_fs_svc_type_id = nas_service_type.nas_svc_type_id",
         "nas_fs.nas_fs_date_delete_asked IS NULL",
-        "nas_fs.nas_fs_name NOT IN (SELECT vol_name FROM nas2020_onboarding)"
+        ("nas_fs_svc_type_id IN ({0})" -f ($volTypeIdList -join ",")),
+        "nas_fs.nas_fs_name NOT IN (SELECT vol_name FROM nas2020_onboarding)",
+        "nas_fs.nas_fs_id=nas_fs_admin_mail.nas_fs_id"
     )
 
     $columns = @(
         "nas_fs.*",
         "nas_service_type.nas_svc_type_name",
-        "infra_vserver.infra_vserver_name"
+        "infra_vserver.infra_vserver_name",
+        "GROUP_CONCAT(nas_fs_admin_mail.nas_fs_admin_mail)AS mailList"
     )
 
     
-    $request = ("SELECT {0} FROM {1} WHERE {2} ORDER BY nas_fs_unit,infra_vserver_name,nas_fs_name" -f `
+    $request = ("SELECT {0} FROM {1} WHERE {2} GROUP BY nas_fs_admin_mail.nas_fs_id ORDER BY nas_fs_unit,infra_vserver_name,nas_fs_name " -f `
                 ($columns -join ","), ($tables -join ","), ($conditions -join " AND "))
 
     $list = $sqldb.execute($request)                
+
 
     return $list
 }
@@ -105,12 +123,20 @@ try
     # Création de l'objet pour logguer les exécutions du script (celui-ci sera accédé en variable globale même si c'est pas propre XD)
     $logHistory = [LogHistory]::new(@('xaas','nas', 'onboarding'), $global:LOGS_FOLDER, 30)
     
-    # On commence par contrôler le prototype d'appel du script
-    . ([IO.Path]::Combine("$PSScriptRoot", "include", "ArgsPrototypeChecker.inc.ps1"))
+    # Objet pour pouvoir envoyer des mails de notification
+	$valToReplace = @{
+		targetEnv = $targetEnv
+		targetTenant = $targetTenant
+    }
+    $notificationMail = [NotificationMail]::new($configGlobal.getConfigValue(@("mail", "admin")), $global:MAIL_TEMPLATE_FOLDER, `
+                                                    ($global:VRA_MAIL_SUBJECT_PREFIX -f $targetEnv, $targetTenant), $valToReplace)
 
     # Ajout d'informations dans le log
     $logHistory.addLine(("Script executed as '{0}' with following parameters: `n{1}" -f $env:USERNAME, ($PsBoundParameters | ConvertTo-Json)))
-    
+
+    # On commence par contrôler le prototype d'appel du script
+    . ([IO.Path]::Combine("$PSScriptRoot", "include", "ArgsPrototypeChecker.inc.ps1"))
+
     # On met en minuscules afin de pouvoir rechercher correctement dans le fichier de configuration (vu que c'est sensible à la casse)
     $targetEnv = $targetEnv.ToLower()
     $targetTenant = $targetTenant.ToLower()
@@ -120,6 +146,8 @@ try
                                 $configNAS.getConfigValue(@($targetEnv, "user")),
                                 $configNAS.getConfigValue(@($targetEnv, "password")))
 
+
+
     # Pour accéder à la base de données
 	$sqldb = [SQLDB]::new([DBType]::MySQL,
                         $configNAS.getConfigValue(@("webisteDB", "host")),
@@ -128,6 +156,12 @@ try
                         $configNAS.getConfigValue(@("webisteDB", "password")),
                         $configNAS.getConfigValue(@("webisteDB", "port")), $false)                                
 
+    $logHistory.addLineAndDisplay("Connecting to vRA...")
+    $vra = [vRAAPI]::new($configVra.getConfigValue(@($targetEnv, "infra", "server")), 
+                            $targetTenant, 
+                            $configVra.getConfigValue(@($targetEnv, "infra", $targetTenant, "user")), 
+                            $configVra.getConfigValue(@($targetEnv, "infra", $targetTenant, "password")))
+
     # Si on doit activer le Debug,
     if(Test-Path (Join-Path $PSScriptRoot "$($MyInvocation.MyCommand.Name).debug"))
     {
@@ -135,13 +169,7 @@ try
         $netapp.activateDebug($logHistory)    
     }
 
-    # Objet pour pouvoir envoyer des mails de notification
-	$valToReplace = @{
-		targetEnv = $targetEnv
-		targetTenant = $targetTenant
-    }
-    $notificationMail = [NotificationMail]::new($configGlobal.getConfigValue(@("mail", "admin")), $global:MAIL_TEMPLATE_FOLDER, `
-                                                    ($global:VRA_MAIL_SUBJECT_PREFIX -f $targetEnv, $targetTenant), $valToReplace)
+    
 
     #$dataFile = ([IO.Path]::Combine("$PSScriptRoot", $dataFile))
     # -------------------------------------------------------------------------
@@ -153,7 +181,7 @@ try
         $ACTION_GEN_DATA_FILE 
         {
             Write-Host -NoNewLine "Getting volume list... "
-            $volList = getVolToOnboard -sqldb $sqldb
+            $volList = getVolToOnboard -sqldb $sqldb -volType $volType
             Write-Host "done" -foregroundColor:DarkGreen
 
             $cols = @(
@@ -162,11 +190,13 @@ try
                 @("Rattachement", "nas_fs_rattachement"),
                 @("vServer", "infra_vserver_name"),
                 @("SizeGB", "nas_fs_quota_gb"),
+                @("Mail List", "mailList"), 
                 "nas2020VolName",
+                "Owner (user@intranet.epfl.ch)",
                 "targetTenantName",
                 "targetBgName")
 
-            (($cols | ForEach-Object{@($_)[0]}) -join ",") | Out-File $dataFile -Encoding:utf8
+            (($cols | ForEach-Object{@($_)[0]}) -join $CSV_SEPARATOR) | Out-File $dataFile -Encoding:utf8
 
             Write-Host -NoNewLine "Extracting data... " 
             # Parcours des volumes récupérés
@@ -188,7 +218,7 @@ try
                     $valList += $val
                 }
 
-                ($valList -join ",") | Out-File $dataFile -Encoding:utf8 -append
+                ($valList -join $CSV_SEPARATOR) | Out-File $dataFile -Encoding:utf8 -append
             }
             Write-Host "done" -foregroundColor:DarkGreen
 
@@ -199,7 +229,26 @@ try
         # -- Importation depuis un fichier de données
         $ACTION_IMPORT
         {
+            $catalogItemName = Switch($volType)
+            {
+                "col" { $XAAS_NAS_ONBOARD_COLL_CATALOG_ITEM }
+                "app" { Throw "Not handled"}
+            }
+        
 
+            $catalogItem = $vra.getCatalogItem($catalogItemName)
+
+            if($null -eq $catalogItem)
+            {
+                Throw ("Catalog Item '{0}' not found on {1}::{2}" -f $catalogItemName, $targetEnv.toUpper(), $targetTenant)
+            }
+
+            $bg = $vra.getBG("epfl_vposi_itop_sddc")
+
+
+            $template = $vra.getCatalogItemRequestTemplate($catalogItem, $bg, "chaboude@intranet.epfl.ch")
+
+            $template
         }
 
     }
@@ -215,21 +264,13 @@ catch
 
 
     $logHistory.addError(("An error occured: `nError: {0}`nTrace: {1}" -f $errorMessage, $errorTrace))
-    
-    # On ajoute les retours à la ligne pour l'envoi par email, histoire que ça soit plus lisible
-    $errorMessage = $errorMessage -replace "`n", "<br>"
 
-    # Création des informations pour l'envoi du mail d'erreur
-    $valToReplace = @{	
-        scriptName = $MyInvocation.MyCommand.Name
-        computerName = $env:computername
-        parameters = (formatParameters -parameters $PsBoundParameters )
-        error = $errorMessage
-        errorTrace =  [System.Net.WebUtility]::HtmlEncode($errorTrace)
+    if($null -ne $vra)
+    {
+        $vra.disconnect()
     }
 
-    # Envoi d'un message d'erreur aux admins 
-    $notificationMail.send("Error in script '{{scriptName}}'", "global-error", $valToReplace) 
+    Throw
 }
 
 if($null -ne $vra)
