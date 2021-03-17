ab9da066342692e9f13905a277fae63d43693c45
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2021-03-17 16:07:34 +0100

    Some adaptations

--- powershell/xaas-sample-endpoint.ps1
+++ powershell/xaas-sample-endpoint.ps1
@@ -155,6 +155,14 @@ try
     # TODO: Adapter la ligne suivante
     #$logHistory = [LogHistory]::new(@('xaas','s3', 'endpoint'), $global:LOGS_FOLDER, 30)
     
+    # Objet pour pouvoir envoyer des mails de notification
+	$valToReplace = @{
+		targetEnv = $targetEnv
+		targetTenant = $targetTenant
+	}
+	$notificationMail = [NotificationMail]::new($configGlobal.getConfigValue(@("mail", "admin")), $global:MAIL_TEMPLATE_FOLDER, `
+												($global:VRA_MAIL_SUBJECT_PREFIX -f $targetEnv, $targetTenant), $valToReplace)
+                                                
     # On commence par contrôler le prototype d'appel du script
     . ([IO.Path]::Combine("$PSScriptRoot", "include", "ArgsPrototypeChecker.inc.ps1"))
 
@@ -192,13 +200,7 @@ try
     }
     
 
-    # Objet pour pouvoir envoyer des mails de notification
-	$valToReplace = @{
-		targetEnv = $targetEnv
-		targetTenant = $targetTenant
-	}
-	$notificationMail = [NotificationMail]::new($configGlobal.getConfigValue(@("mail", "admin")), $global:MAIL_TEMPLATE_FOLDER, `
-												($global:VRA_MAIL_SUBJECT_PREFIX -f $targetEnv, $targetTenant), $valToReplace)
+    
 
 
     # En fonction de l'action demandée
@@ -216,6 +218,10 @@ try
         # -- Effacement d'un bucket
         $ACTION_DELETE {
 
+        }
+
+        default {
+            Throw ("Action '{0}' not supported" -f $action)
         }
 
         # TODO: Compléter avec la liste des actions définies précédemment
